a1e7f59b6a8f8529ef50eb0a5837c88383335430
rithikkulkarni <rithik.r.kulkarni@gmail.com>
2026-06-11 16:00:08 -0400

    Added demo for tabletalk

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -122,7 +122,7 @@ export function Projects() {
     image: 'https://images.unsplash.com/photo-1666875753105-c63a6f3bdc86?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tags: ['Large Languages Models', 'Prompt Engineering', 'TypeScript', 'React', 'APIs'],
     github: 'https://github.com/rithikkulkarni/tabletalk',
-    demo: '#',
+    demo: 'https://tabletalk-delta.vercel.app',
     featured: true,
   },
 ];
